9c37548dac0520eccdf1a12fcc2bc00cfaac3b86
Charbel Mansour <109873765+charbelmansour005@users.noreply.github.com>
2022-08-01 11:16:10 +0300

    jdid jdid

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
-import React from "react";
-import Card from "./Card";
-//function products
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./Pages/Home";
+import NavigationPage from "./Pages/NavigationPage";
+import ErrorPage from "./Pages/ErrorPage";
+import Dishes from "./Pages/Dishes";
+import Starters from "./Pages/Starters";
+import Hotdrinks from "./Pages/Hotdrinks";
+import Colddrinks from "./Pages/Colddrinks";
+import Arguileh from "./Pages/Arguileh";
+import Sandwhiches from "./Pages/Sandwhiches";
+
 function App() {
-  const products = [
-    { name: "vest", price: 50 },
-    { name: "jacket", price: 70 },
-    { name: "sweater", price: 20 },
-    { name: "tshirt", price: 40 },
-    { name: "necklace", price: 70 },
-    { name: "jumper", price: 40 },
-  ];
   return (
-    <div>
-      {products.map((product) => (
-        <Card name={product.name} price={product.price} />
-      ))}
+    <div style={{ textAlign: "center" }}>
+      <Router>
+        <nav>
+          <Link to="/"> Home </Link>
+          <Link to="/navigationpage"> NavigationPage </Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/navigationpage" element={<NavigationPage />} />
+          <Route path="/dishes" element={<Dishes />} />
+          <Route path="/starters" element={<Starters />} />
+          <Route path="/hotdrinks" element={<Hotdrinks />} />
+          <Route path="/colddrinks" element={<Colddrinks />} />
+          <Route path="/arguileh" element={<Arguileh />} />
+          <Route path="/sandwhiches" element={<Sandwhiches />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
